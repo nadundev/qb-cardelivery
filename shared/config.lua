420ba058616232d3b0c63a3c76519dfe7d86b527
nadundev <nadunnissankauiux@gmail.com>
2024-09-17 14:01:16 +0530

    testing chords added

--- shared/config.lua
+++ shared/config.lua
@@ -2,13 +2,11 @@ Config = {}
 
 Config.PayPhoneLocation = vector3(-664.59, -671.46, 31.42) -- Location of the pay phone to start the mission
 Config.ExoticCarLocations = {
-    vector4(-1477.71, 520.84, 115.38, 285.94),
-    vector4(-1805.39, 438.51, 128.37, 125.67),
-    vector4(-1363.33, 624.6, 133.89, 25.92),
+    vector4(-1086.01, -301.3, 37.27, 260.59),
     -- Add more locations as needed
 }
 
-Config.DeliveryLocation = vector3(1173.67, 2634.37, 37.75) -- Location where the player needs to deliver the car
+Config.DeliveryLocation = vector3(-1260.75, -225.41, 42.02) -- Location where the player needs to deliver the car
 Config.DeliveryZoneRadius = 10.0 -- Radius of the delivery zone
 
 Config.DeliveryMarkerType = 27 -- Marker type for the delivery location (https://docs.fivem.net/docs/game-references/markers/)
